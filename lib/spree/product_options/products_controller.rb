d0a8e3824428198499577266f7c06e779bbee5d8
Steph Powell <steph@endpoint.com>
2010-06-22 11:24:56 -0600

    Fixing sort logic. Removing debug comments.

--- lib/spree/product_options/products_controller.rb
+++ lib/spree/product_options/products_controller.rb
@@ -3,7 +3,6 @@ module Spree::ProductOptions::ProductsController
     controller.append_before_filter :define_2d_option_matrix, :only => :show 
   end
   def define_2d_option_matrix
-logger.warn "steph inside here!"
     variants = Spree::Config[:show_zero_stock_products] ?
       object.variants.active.select { |a| !a.option_values.empty? } :
       object.variants.active.select { |a| !a.option_values.empty? && a.in_stock }
@@ -13,7 +12,6 @@ logger.warn "steph inside here!"
     variant_ids = {}
     sizes = []
     colors = []
-logger.warn "steph inside here! 1"
     variants.each do |variant|
       active_size = variant.option_values.select { |a| a.option_type.presentation == 'PO_Size' }.first
       active_color = variant.option_values.select { |a| a.option_type.presentation == 'PO_Color' }.first
@@ -22,7 +20,7 @@ logger.warn "steph inside here! 1"
       colors << active_color
     end
     size_sort = Hash['S', 0, 'M', 1, 'L', 2, 'XL', 3]
-    @sc_matrix = { 'sizes' => sizes.sort_by { |s| size_sort[s.presentation] }.uniq,
+    @sc_matrix = { 'sizes' => sizes.sort_by { |s| size_sort[s.presentation] || 0 }.uniq,
         'colors' => colors.uniq,
         'variant_ids' => variant_ids }
   end
